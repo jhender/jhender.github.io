e93ea809401bfad06cab7d32a77e442c086c07f8
Jh <jianhui@gmail.com>
2024-09-09 23:54:01 +0800

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jhender.github.io',
+    site: 'https://jhender.com',
     integrations: [
         mdx(),
         sitemap(),
